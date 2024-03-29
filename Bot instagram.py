1553942d687aea69160d1ed2fc9fd8ef05a6b48b
dexgame4006 <165038421+dexgame4006@users.noreply.github.com>
2024-03-29 16:04:30 +0000

    Change of names

--- Bot instagram.py
+++ Bot instagram.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
 import time
 
+x=input('Username:')
+y=input('Password:')
+
 navegador = webdriver.Chrome()
 navegador.get('https://www.instagram.com')
 time.sleep(1.5)
 navegador.find_element_by_xpath('/html/body/div[4]/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[1]').click()
 time.sleep(1)
-navegador.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys('dexgame4006@gmail.com')
-navegador.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys('juxKah-sudvun-xowzy7')
+navegador.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(x)
+navegador.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(y)
 time.sleep(1)
 navegador.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]').click()
 
